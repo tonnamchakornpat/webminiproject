c6b67c6c82026c7360df9af11b505931840f6f41
chakornpat <chakornpat0206@outlook.com>
2023-07-17 10:32:44 +0700

    Refactor login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,25 +1,55 @@
+import { useState } from 'react'
 import '../styles/scss/main.scss'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 
 function LoginPage() {
+  const [accNewStatus, setAccNewStatus] = useState(false)
+  const showRegisForm = () => setAccNewStatus(!accNewStatus)
+
   return (
     <div className="containerCard">
       <div className="loginCard">
-        <h2>Login</h2>
+        {accNewStatus && (
+          <AiOutlineArrowLeft
+            id="backIcon"
+            size={30}
+            style={{
+              marginBottom: '10px',
+              position: 'relative',
+            }}
+            onClick={showRegisForm}
+          />
+        )}
+        <h2> {accNewStatus ? 'Create New Account' : 'Login'}</h2>
         <h3>Enter Your Credentials</h3>
         <form action="/" method="post">
           <input
+            autoComplete="off"
             type="text"
             name="username"
             id="username"
             placeholder="Username"
           />
           <input
+            autoComplete="off"
             type="password"
             name="password"
             id="password"
-            placeholder="password"
+            placeholder="Password"
           />
-          <a href="/">Create new account</a>
+          <input
+            autoComplete="off"
+            type="password"
+            name="conPassword"
+            id="conPassword"
+            placeholder="Confirm Password"
+            style={{
+              display: accNewStatus ? 'block' : 'none',
+              position: accNewStatus ? '-10px' : '0px',
+            }}
+          />
+          {!accNewStatus && <p onClick={showRegisForm}>Create new account</p>}
+
           <button type="submit">Enter</button>
         </form>
       </div>
