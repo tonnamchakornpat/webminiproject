3cee011ba646a644869d0facbb15dda84dad7808
chakornpat <chakornpat0206@outlook.com>
2023-07-19 20:08:38 +0700

    Refactor :sparkles: Add Post Component And Refactor Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,47 +4,41 @@ import { AiOutlineClose } from 'react-icons/ai'
 import { useState } from 'react'
 
 function Navbar() {
-  const [MenuStatus, setMenuStatus] = useState(true)
+  const [MenuStatus, setMenuStatus] = useState(false)
   const changeStatus = () => {
     setMenuStatus(!MenuStatus)
   }
+  const styleMenu = {
+    opacity: '1',
+    visibility: 'visible',
+  }
 
   return (
-    <div className="Navbar">
-      <div className="NavbarContainer">
-        <div className="itemsContainer">
-          <nav>
-            <h1>Header</h1>
-            {MenuStatus ? (
-              <AiOutlineClose size={'1.5rem'} onClick={changeStatus} />
-            ) : (
-              <GiHamburgerMenu size={'1.5rem'} onClick={changeStatus} />
-            )}
-          </nav>
+    <nav>
+      <div className="container">
+        <div className="logo">
+          <h1>Hello</h1>
         </div>
-      </div>
-
-      <ul
-        style={
-          !MenuStatus
-            ? { position: 'relative', left: '200vw' }
-            : { position: 'relative' }
+        {
+          <GiHamburgerMenu
+            size={'1.5rem'}
+            className="toggleBtn"
+            onClick={changeStatus}
+          />
         }
-      >
-        <li>
-          <RiFileList2Line className="iconMenu" />
-          <p>My Post</p>
-        </li>
-        <li>
-          <RiEdit2Line className="iconMenu" />
-          <p>Write</p>
-        </li>
-        <li>
-          <RiLogoutBoxRLine className="iconMenu" />
-          <p>Logout</p>
-        </li>
-      </ul>
-    </div>
+        <ul className="list" style={MenuStatus ? styleMenu : null}>
+          <a href="#">
+            <li>My Post</li>
+          </a>
+          <a href="#">
+            <li>Create New</li>
+          </a>
+          <a href="#">
+            <li>Logout</li>
+          </a>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,41 @@
 import '../styles/scss/main.scss'
 import Navbar from '../components/Navbar'
+import Post from '../components/Post'
 
+const posts = [
+  {
+    postId: 1234,
+    name: 'User1234',
+    title: 'Title',
+    content:
+      'Eos excepturi ipsa aut. Quia tempora inventore sunt quo impedit. Nemo quia repellat nesciunt dolores ipsa consequatur eos.',
+    time: '2 june 2022',
+  },
+  {
+    postId: 1222,
+    name: 'User1234',
+    title: 'Title',
+    content:
+      'Eos excepturi ipsa aut. Quia tempora inventore sunt quo impedit. Nemo quia repellat nesciunt dolores ipsa consequatur eos.',
+    time: '2 june 2022',
+  },
+]
 function HomePage() {
   return (
     <>
       <Navbar />
-      <main></main>
+      <main>
+        <section className="contents">
+          {posts.map((post) => (
+            <Post
+              postId={post.postId}
+              title={post.title}
+              name={post.name}
+              datetime={post.time}
+            />
+          ))}
+        </section>
+      </main>
     </>
   )
 }
